31afbedb455ad286908e7e571cf5524904fc8566
Koles <koles.near@protonmail.com>
2023-08-07 18:15:16 +0200

    EU country checker ENUM

---
+++ app/Enums/EuCountryEnum.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Enums;
+
+enum EuCountryEnum
+{
+    const EU_COUNTRIES = [
+        'AT',
+        'BE',
+        'BG',
+        'CY',
+        'CZ',
+        'DE',
+        'DK',
+        'EE',
+        'ES',
+        'FI',
+        'FR',
+        'GR',
+        'HR',
+        'HU',
+        'IE',
+        'IT',
+        'LT',
+        'LU',
+        'LV',
+        'MT',
+        'NL',
+        'PO',
+        'PT',
+        'RO',
+        'SE',
+        'SI',
+        'SK',
+    ];
+
+    public static function check(String $countryCode): bool
+    {
+        return in_array(strtoupper($countryCode), self::EU_COUNTRIES);
+    }
+}
+
