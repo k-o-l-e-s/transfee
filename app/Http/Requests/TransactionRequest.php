0b91e1b85e6baf74d028a0b2f8f723eb3d676fac
Koles <koles.near@protonmail.com>
2023-08-08 20:24:42 +0200

    form request

---
+++ app/Http/Requests/TransactionRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Transaction;
+use Illuminate\Foundation\Http\FormRequest;
+use App\Enums\CurrencyEnum;
+
+class TransactionRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'bin' => ['required', 'digits_between:5,8'],
+            'amount' => ['required', 'numeric'],
+            'currency' => [
+                'required',
+                'size:3',
+                function ($attribute, $value, $fail) {
+                    if (!CurrencyEnum::check($value)) {
+                        $fail("The $attribute is invalid.");
+                    }
+                },
+            ],
+        ];
+    }
+
+    public function store(TransactionRequest $request)
+    {
+        // The request has already been validated at this point
+        $transaction = Transaction::create($request->all());
+
+        // ... rest of your code
+    }
+
+}
