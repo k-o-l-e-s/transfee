f5bd68b09fd22fff39887e663a5bf50ccbf0ca00
Koles <koles.near@protonmail.com>
2023-08-08 20:25:16 +0200

    artisan command added

---
+++ app/Console/Commands/FileFeeCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\File;
+
+class FileFeeCommand extends Command
+{
+    protected $signature = 'app:filefee {file_name}';
+
+    protected $description = 'Calculate fees for transactions from a file';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $fileName = $this->argument('file_name');
+
+        if (!File::exists($fileName)) {
+            $this->error("File not found: $fileName");
+            return;
+        }
+
+        $lines = file($fileName, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            $transactionData = json_decode($line, true);
+
+            $transaction = new Transaction($transactionData);
+
+            $transaction->calculateFee();
+
+//            $this->info($transaction->bin." ".$transaction->bin_country." ".$transaction->fee." ".$transaction->amount);
+            $this->info($transaction->fee);
+        }
+    }
+}
